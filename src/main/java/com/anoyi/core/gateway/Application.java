d15c74967d20c0b877691c99622af7e358ff1f46
anoy <545544032@qq.com>
2018-07-09 13:09:50 +0800

    update

--- src/main/java/com/anoyi/core/gateway/Application.java
+++ src/main/java/com/anoyi/core/gateway/Application.java
@@ -1,11 +1,10 @@
 package com.anoyi.core.gateway;
 
-import com.anoyi.BootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application extends BootApplication {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
